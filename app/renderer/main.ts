8a927adbadb2290f50130a46d565e3e5dfaca9d8
huguogang <huguogang@g.email.noreply>
2016-05-02 21:44:12 -0700

    enable prod mode

--- app/renderer/main.ts
+++ app/renderer/main.ts
@@ -2,5 +2,5 @@ import {enableProdMode} from 'angular2/core';
 import {bootstrap}    from 'angular2/platform/browser';
 import {AppComponent} from './app.component';
 
-// enableProdMode();
+enableProdMode();
 bootstrap(AppComponent);
